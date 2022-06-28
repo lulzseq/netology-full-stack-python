27ce9b2588f26726e515b3f9ba75633680d6ce30
Dmitry Kuznetsov <teoahoo@gmail.com>
2022-06-28 19:45:50 +0300

    Add a new homework files

--- 08. Django/03. ORM (part 2)/orm_migrations/school/views.py
+++ 08. Django/03. ORM (part 2)/orm_migrations/school/views.py
@@ -1,15 +1,11 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from .models import Student
+from .models import Teacher
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    context = {'object_list': Teacher.objects.all().prefetch_related('students')}
 
     return render(request, template, context)
