ebb58eb57f6d572edb6e38bc6ddd2bacd043c74e
Dmitry Kuznetsov <teoahoo@gmail.com>
2022-06-29 22:46:23 +0300

    Minor edits

--- 08. Django/03. ORM (part 2)/m2m-relations/articles/models.py
+++ 08. Django/03. ORM (part 2)/m2m-relations/articles/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Title')
     text = models.TextField(verbose_name='Text')
     published_at = models.DateTimeField(verbose_name='Publication date')
-    image = models.ImageField(null=True, blank=True, verbose_name='Image',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Image', )
 
     class Meta:
         verbose_name = 'Article'
@@ -14,15 +14,18 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
+
 class Tag(models.Model):
     name = models.CharField(max_length=50)
     article = models.ManyToManyField(Article, through='ArticleScope')
+
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
+
 
 class ArticleScope(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
--- 08. Django/03. ORM (part 2)/m2m-relations/articles/views.py
+++ 08. Django/03. ORM (part 2)/m2m-relations/articles/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-from articles.models import Article, ArticleScope
+from articles.models import Article, ArticleScope, Tag
 from django.db.models.query import Prefetch
 
 
 def articles_list(request):
-    template = 'articles/news.html'
+    tag = request.GET.get('tag')
+    tag_list = Tag.objects.all().order_by('name')
 
-    ordering = '-published_at'
-    object_list = Article.objects.order_by(ordering).prefetch_related(
-        Prefetch('scopes', ArticleScope.objects.order_by('-is_main', 'tag__name'))
-    )
+    if tag:
+        object_list = Article.objects.filter(tag=tag).order_by('-published_at').prefetch_related(
+            Prefetch('scopes', ArticleScope.objects.order_by('-is_main', 'tag__name')))
+    else:
+        object_list = Article.objects.order_by('-published_at').prefetch_related(
+            Prefetch('scopes', ArticleScope.objects.order_by('-is_main', 'tag__name')))
+
     context = {
-        'object_list': object_list
+        'object_list': object_list,
+        'tag_list': tag_list
     }
 
-    return render(request, template, context)
+    return render(request, 'articles/news.html', context)
