9e64c4fa0f478b39126230936c5ef9638d025298
Dmitry Kuznetsov <teoahoo@gmail.com>
2022-07-18 01:04:18 +0300

    Add final files

--- 07. Django/06. CRUD in DRF/logistic/admin.py
+++ 07. Django/06. CRUD in DRF/logistic/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Stock, StockProduct
 
-# Register your models here.
+
+class StockInline(admin.TabularInline):
+    model = Stock.products.through
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [StockInline]
+
+
+class StockProductInline(admin.TabularInline):
+    model = StockProduct
+    extra = 1
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    inlines = [StockProductInline]
