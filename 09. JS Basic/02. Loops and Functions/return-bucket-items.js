fc2b2040086d6046d0bc09003e135b6e624bcb21
Dmitry Kuznetsov <teoahoo@gmail.com>
2022-11-13 22:34:50 +0800

    Add part of #2 work

---
+++ 09. JS Basic/02. Loops and Functions/return-bucket-items.js
@@ -0,0 +1,43 @@
+let goods = {
+    1: {
+        id: 1,
+        name: 'Телевизор',
+        description: 'Описание телевизора',
+        sizies: ['small', 'medium', 'large'],
+        price: 500,
+        available: false,
+    },
+    2: {
+        id: 2,
+        name: 'Телефон',
+        description: 'Описание телефона',
+        sizies: ['small', 'large'],
+        price: 300,
+        available: true,
+    },
+    3: {
+        id: 3,
+        name: 'Ноутбук',
+        description: 'Описание ноутбука',
+        sizies: ['small', 'large'],
+        price: 900,
+        available: true,
+    },
+    4: {
+        id: 4,
+        name: 'Кондиционер',
+        description: 'Описание кондиционера',
+        sizies: ['medium', 'large'],
+        price: 700,
+        available: false,
+    },
+    5: {
+        id: 5,
+        name: 'Колонка',
+        description: 'Описание колонки',
+        sizies: ['small', 'medium'],
+        price: 100,
+        available: true,
+    }
+}
+
