113d55a3df80d7ffb69a57d57864ab237cff8af9
Dmitry Kuznetsov <teoahoo@gmail.com>
2022-12-04 23:40:09 +0800

    Homework is done

---
+++ 09. JS Basic/05. JS in browsers/02. Cookie clicker/task.js
@@ -0,0 +1,46 @@
+function counter(start=0) {
+    let _next = start;
+    return function increment() {
+        _next++;
+        return _next;
+    }
+}
+
+function nowSeconds() {
+    const seconds = new Date().getTime() / 1000;
+    return seconds;
+}
+
+const size = 50;
+const clickRate = 0;
+let secondsInterval = 0;
+
+const addClick = counter();
+
+const img = document.getElementById('cookie');
+const countCliker = document.getElementById('clicker__counter');
+const clickerRate = document.getElementById('clicker__rate');
+
+img.onclick = () => {
+    countCliker.textContent = addClick();
+
+    if (countCliker.textContent == 1) {        
+        secondsInterval = nowSeconds();
+        clickerRate.textContent = 1;
+    } else if (!isNaN(countCliker.textContent)) {
+        const now = nowSeconds();
+        const value = 1 / (now - secondsInterval);
+        
+        clickerRate.textContent = new Intl.NumberFormat('ru', { minimumFractionDigits: 4}).format(value);
+        
+        secondsInterval = now;
+    }
+
+    img.width += size;
+    img.height += size;
+
+    setTimeout(function pause() {
+        img.width -= size;
+        img.height -= size;
+    }, 100)
+}
